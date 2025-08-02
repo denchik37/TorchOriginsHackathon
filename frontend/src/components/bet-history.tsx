8f68d0c36e0c28cd5e505f35adbaeaebf23542d6
Sebastian Balaj <33859946+balajsebastian@users.noreply.github.com>
2025-08-02 12:43:14 +0300

    feat: display whole address in tooltip

--- frontend/src/components/bet-history.tsx
+++ frontend/src/components/bet-history.tsx
@@ -2,6 +2,7 @@
 
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Tooltip } from "@/components/ui/tooltip";
 import { formatAddress } from "@/lib/utils";
 
 interface BetHistoryProps {
@@ -12,7 +13,7 @@ interface BetHistoryProps {
 const mockBetHistory = [
 	{
 		id: 1,
-		user: "0xe7a...72d",
+		user: "0xAb5801a7D398351b8bE11C439e05C5b3259aec9B",
 		amount: 1.67,
 		range: "0.27-0.28",
 		date: "Aug 1, 13:00",
@@ -20,7 +21,7 @@ const mockBetHistory = [
 	},
 	{
 		id: 2,
-		user: "0x908...ef0",
+		user: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
 		amount: 10.12,
 		range: "0.1-0.2",
 		date: "Aug 8, 00:00",
@@ -28,7 +29,7 @@ const mockBetHistory = [
 	},
 	{
 		id: 3,
-		user: "0x458...ef1",
+		user: "0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF",
 		amount: 431.54,
 		range: "0.37-0.5",
 		date: "Aug 1, 23:59",
@@ -36,7 +37,7 @@ const mockBetHistory = [
 	},
 	{
 		id: 4,
-		user: "0x271...415",
+		user: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
 		amount: 0.78,
 		range: "0.24-0.24",
 		date: "Aug 3, 21:15",
@@ -44,7 +45,7 @@ const mockBetHistory = [
 	},
 	{
 		id: 5,
-		user: "0x203...C6E",
+		user: "0x1234567890abcdef1234567890abcdef12345678",
 		amount: 1.36,
 		range: "1-2",
 		date: "Dec 31, 20:02",
@@ -86,9 +87,12 @@ export function BetHistory({ className }: BetHistoryProps) {
 												{bet.avatar}
 											</AvatarFallback>
 										</Avatar>
-										<span className="text-sm font-mono text-light-gray">
-											{bet.user}
-										</span>
+
+										<Tooltip content={bet.user}>
+											<span className="text-sm font-mono text-light-gray">
+												{formatAddress(bet.user)}
+											</span>
+										</Tooltip>
 									</div>
 								</td>
 								<td className="py-3 px-4 text-sm text-light-gray">
