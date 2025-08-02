0687046cb19bad4ef5f76a4c9f8e124014c80c2d
Sebastian Balaj <33859946+balajsebastian@users.noreply.github.com>
2025-08-02 12:42:48 +0300

    feat: tooltip

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,22 +1,30 @@
-export { Button, buttonVariants } from './button'
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card'
-export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs'
-export { Input } from './input'
-export { Avatar, AvatarFallback, AvatarImage } from './avatar'
+export { Button, buttonVariants } from "./button";
 export {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from './dropdown-menu' 
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "./card";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
+export { Input } from "./input";
+export { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+export {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuPortal,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
+} from "./dropdown-menu";
+export { Tooltip } from "./tooltip";
